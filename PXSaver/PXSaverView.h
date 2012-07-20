9372a01c039aacfb62576cd871bc6d6309add44d
Ash Furrow <ash@500px.com>
2012-07-20 10:10:21 -0400

    Initial Commit

---
+++ PXSaver/PXSaverView.h
@@ -0,0 +1,13 @@
+//
+//  PXSaverView.h
+//  PXSaver
+//
+//  Created by Ash Furrow on 12-07-20.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PXSaverView : ScreenSaverView
+
+@end
---
+++ PXSaver/PXSaverView.m
@@ -0,0 +1,52 @@
+//
+//  PXSaverView.m
+//  PXSaver
+//
+//  Created by Ash Furrow on 12-07-20.
+//  Copyright (c) 2012 500px. All rights reserved.
+//
+
+#import "PXSaverView.h"
+
+@implementation PXSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
